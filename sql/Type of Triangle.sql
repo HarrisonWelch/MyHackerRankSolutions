b679aa3cb720794322900dd6d9a31417c21c7034
Harrison Welch <Harrison.Welch@SSINA.BIZ>
2020-01-29 20:16:10 -0600

    Solved Types of Triangles

--- sql/Type of Triangle.sql
+++ sql/Type of Triangle.sql
@@ -4,3 +4,13 @@
 -- Isosceles: It's a triangle with  sides of equal length.
 -- Scalene: It's a triangle with  sides of differing lengths.
 -- Not A Triangle: The given values of A, B, and C don't form a triangle.
+
+SELECT
+CASE
+    WHEN A+B <= C OR B+C <= A OR C+A <= B THEN 'Not A Triangle'
+    WHEN A = B AND B = C THEN 'Equilateral'
+    WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+    ELSE 'Scalene'
+END
+FROM
+    TRIANGLES;
