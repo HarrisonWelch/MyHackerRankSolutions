97c3a5fc0bbfaed19c84d39ccb53b397bd2995ca
HarrisonWelch <harrison.r.welch@gmail.com>
2020-01-30 01:52:05 -0600

    Solved Top Earners

---
+++ sql/Top Earners.sql
@@ -0,0 +1,19 @@
+-- We define an employee's total earnings to be their monthly salary*months
+-- worked, and the maximum total earnings to be the maximum total earnings 
+-- for any employee in the Employee table. Write a query to find the maximum 
+-- total earnings for all employees as well as the total number of employees 
+-- who have maximum total earnings. Then print these values as  space-separated integers.
+
+SELECT * FROM
+(
+    SELECT
+        salary*months,
+        COUNT(*)
+    FROM
+        EMPLOYEE
+    GROUP BY
+        salary*months
+    ORDER BY
+        salary*months DESC
+)
+WHERE ROWNUM = 1;
