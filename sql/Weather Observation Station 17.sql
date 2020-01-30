6673f75e3d6eb1ecd5a4a3b51726d461c7547b2b
HarrisonWelch <harrison.r.welch@gmail.com>
2020-01-30 02:28:13 -0600

    Weather Observation Station 17

---
+++ sql/Weather Observation Station 17.sql
@@ -0,0 +1,15 @@
+-- Query the Western Longitude (LONG_W)where the smallest Northern Latitude 
+-- (LAT_N) in STATION is greater than 38.7780. Round your answer to  decimal places.
+
+SELECT ROUND(LONG_W,4) FROM
+(
+    SELECT
+        *
+    FROM
+        STATION
+    WHERE
+        LAT_N > 38.7780
+    ORDER BY
+        LAT_N
+)
+WHERE ROWNUM=1;
