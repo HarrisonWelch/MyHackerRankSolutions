5d87a081d71a06201ed19da4564cb2504f2e6fcc
HarrisonWelch <hwelch1@my.apsu.edu>
2020-02-02 22:55:47 -0600

    Solved Draw Triangle 1

---
+++ sql/Draw Triangle 1.sql
@@ -0,0 +1,26 @@
+-- P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+-- * * * * * 
+-- * * * * 
+-- * * * 
+-- * * 
+-- *
+
+-- Write a query to print the pattern P(20).
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT
+    RPAD('*',x,' *')
+FROM
+    (SELECT 
+        LEVEL x 
+     FROM DUAL 
+     CONNECT BY 
+        LEVEL <= 40 
+     ORDER BY LEVEL DESC)
+WHERE
+    MOD(x,2) = 0;
