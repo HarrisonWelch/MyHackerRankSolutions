4751a5ee7b27a5bc607b1d70faedf3ad219d150c
HarrisonWelch <hwelch1@my.apsu.edu>
2020-02-12 06:48:34 -0600

    Solved Cat and Mouse and Find Digits

---
+++ problem_solving/Find Digits.java
@@ -0,0 +1,50 @@
+// https://www.hackerrank.com/challenges/find-digits/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+        String sn = Integer.toString(n);
+        int sum = 0;
+        for (int i = 0; i < sn.length(); i++) {
+            if (sn.charAt(i) == '0') {
+                continue;
+            }
+            if ((n % Integer.parseInt(String.valueOf(sn.charAt(i)))) == 0) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
