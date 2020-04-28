5b08cb58e1edfe437fea3bc3888bdcf53ac844a6
HarrisonWelch <harrison.r.welch@gmail.com>
2020-04-28 07:33:45 -0500

    Completed Sort Command 1,2,3,4,5,6,7

---
+++ linux_shell/Sort_Command_6.sh
@@ -0,0 +1,3 @@
+# https://www.hackerrank.com/challenges/text-processing-sort-6/problem
+
+sort -t$'\t' -n -k 2
---
+++ linux_shell/Sort_Command_7.sh
@@ -0,0 +1,3 @@
+# https://www.hackerrank.com/challenges/text-processing-sort-7/problem
+
+sort -t$'|' -nr -k 2
