26c6dbc0dd8d959146cfb105c722097f047497f9
HarrisonWelch <harrison.r.welch@gmail.com>
2020-04-29 05:21:54 -0500

    Completed Sed 2

---
+++ linux_shell/Sed_2.sh
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-sed-command-2/problem
+
+sed -e 's/[tT][hH][yY]\b/your/g'
+
+# Replace the whole word thy with your. Case INSENSITIVE!
