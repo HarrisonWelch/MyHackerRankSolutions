53a5cf3e3babc2b95215c0cd0cdcd7bf43d9a4c8
HarrisonWelch <harrison.r.welch@gmail.com>
2020-05-03 18:07:02 -0500

    Working on the FaFRTB code

--- linux_shell/Functions_and_Fractals_Recursive_Trees_Bash!.sh
+++ linux_shell/Functions_and_Fractals_Recursive_Trees_Bash!.sh
@@ -1,9 +1,40 @@
+#!/bin/bash
+
+# Y-fractal
+# Input = 0 | 0 length stem/branch
+# Input = 1 | 16 length stem/branch
+# Input = 2 | 8 length stem/branch
+# Input = 3 | 4 length stem/branch
+# Input = 4 | 2 length stem/branch
+# Input = 5 | 1 length stem/branch
+
 read iter
 
-for i in {1..63};do
-    # echo "YEET"
-    for j in {1..100}; do
-        printf "_"
+# Setup the map string
+map=''
+one=1
+widthMap=63
+heightMap=100
+
+change_string_at_x_y_to_1 () {
+    local spos=0
+    spos=$(($widthMap+2))
+    spos=$(($spos * $2))
+    spos=$(($spos + $1))
+    # echo -e ${map:$1:1}
+    echo $spos
+}
+
+for (( i=$one; i<=$widthMap; i++ ))
+do
+    # echo "YEET $i"
+    for (( j=$one; j<=$widthMap; j++ ))
+    do
+        map+="_"
     done
-    printf "\n"
+    map+="\n"
 done
+
+change_string_at_x_y_to_1 63 50
+
+# echo -e $map
