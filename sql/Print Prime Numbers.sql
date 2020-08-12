586590c6c95bd3180980171d2bf975622c20604b
HarrisonWelch <hwelch1@my.apsu.edu>
2020-08-12 11:25:00 -0500

    Started work on Print Prime Numbers.sql

---
+++ sql/Print Prime Numbers.sql
@@ -0,0 +1,21 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+-- begin
+--     for record in (
+--     select 2 from dual)
+--     loop
+--         dbms_output.put_line("HEY LISTEN");
+--     end loop;
+--     dbms_output.put_line("HEY LISTEN");
+-- end;
+
+-- select 2 from dual;
+
+select
+    listagg(lvl, '&') within group (order by lvl)
+from
+    (select level lvl from dual connect by (level <= 1000 and mod(level,2) <> 0));
+
