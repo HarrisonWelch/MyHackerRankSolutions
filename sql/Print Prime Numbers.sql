eb0d9c5e19bc45aa8d25d81868d83a2db34f489a
HarrisonWelch <harrison.r.welch@gmail.com>
2021-07-10 06:52:14 -0500

    Completed Print Prime Numbers

--- sql/Print Prime Numbers.sql
+++ sql/Print Prime Numbers.sql
@@ -3,27 +3,21 @@ Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
 
--- begin
---     for record in (
---     select 2 from dual)
---     loop
---         dbms_output.put_line("HEY LISTEN");
---     end loop;
---     dbms_output.put_line("HEY LISTEN");
--- end;
-
--- select 2 from dual;
-
--- select
---     listagg(lvl, '&') within group (order by lvl)
--- from
---     (select level lvl from dual connect by (level <= 1000 and mod(level,2) <> 0));
-
 SELECT
-    
-SELECT
-    LEVEL
+    listagg(t3.n, '&') within group (order by t3.n) -- <== combine the numbers in a list
 FROM
-    DUAL
-CONNECT BY
-    LEVEL <= 1000;
+    (SELECT level n from dual connect by level <= 1000) t3 -- <== Get the numbers 1 to 1000
+WHERE
+    t3.n NOT IN ( -- <== If not in the list of composite numbers
+        SELECT DISTINCT
+            t1.n
+        FROM
+            (SELECT level n from dual connect by level <= 1000) t1
+            JOIN (SELECT level n from dual connect by level <= 1000) t2
+                ON t2.n > 1
+                AND t1.n > t2.n
+                AND t1.n / t2.n = trunc(t1.n / t2.n) -- <== Call up two tables of 1 to 1000, if the join is made, its composite (inverted later on)
+    )
+    AND t3.n <> 1 -- <== 1 is not prime by definition
+    AND t3.n <> 0 -- <== 0 is not prime by definition
+;
