ec3af5b06ce4f0b112416c7afa3556037d4688f1
HarrisonWelch <harrison.r.welch@gmail.com>
2021-08-21 21:03:10 -0500

    Completed Java Subarray

--- java/Java Subarray.java
+++ java/Java Subarray.java
@@ -15,5 +15,21 @@ public class Solution {
         for (int i = 0; i < n; i++) {
             ints[i] = scanner.nextInt();
         }
+        int count = 0;
+        int subArraySum = 0;
+        for (int i = 1; i < (n+1); i++) { // size of the array being summed
+            for (int j = 0; j < (n-i+1); j++) { // start/stop of array being summed
+                for (int k = j; k < (j+i); k++) { // iterator through the array
+                    subArraySum += ints[k];
+                    // System.out.println("i = " + i + ", j = " + j + ", k = " + k);
+                }
+                // System.out.println("subArraySum = " + subArraySum);
+                if (subArraySum < 0) {
+                    count++;
+                }
+                subArraySum = 0;
+            }
+        }
+        System.out.println(count);
     }
 }
