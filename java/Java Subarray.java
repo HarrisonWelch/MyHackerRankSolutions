e4e394eee81889c793ae9740986f88225c25fd5a
HarrisonWelch <harrison.r.welch@gmail.com>
2021-08-21 20:00:53 -0500

    Completed Java Subarray

---
+++ java/Java Subarray.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] ints = new int[n];
+        int arraySum = 0;
+        for (int i = 0; i < n; i++) {
+            ints[i] = scanner.nextInt();
+        }
+    }
+}
