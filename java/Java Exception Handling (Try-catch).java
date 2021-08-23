dae1a1e52fdcf6b1ca34564164e56198ad7937da
HarrisonWelch <harrison.r.welch@gmail.com>
2021-08-22 19:22:54 -0500

    Completed Java Exception Handling (Try-catch)

---
+++ java/Java Exception Handling (Try-catch).java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            System.out.println(x/y);
+        } catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        } catch (ArithmeticException e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        }
+    }
+}
